0f8c16410f54d0261bef72a3df643c841132fa41
Basit-Bashir <wbhat56@gmail.com>
2023-05-26 15:06:41 +0530

    initial commit

---
+++ src/Pages/authentication/Login/Login.js
@@ -0,0 +1,36 @@
+import React from "react";
+import classes from "./Login.module.css";
+const Login = () => {
+  return (
+    <>
+      <div className={classes.mainDiv}>
+        <h1 className="logo">Welcome to NorthStar</h1>
+        <div className={classes.container}>
+          <h3 className="logo-3">Login</h3>
+          <form>
+            <div className={classes.user}>
+              <label>Username</label>
+              <input type="text" placeholder="NorthStar"></input>
+            </div>
+            <div className={classes.userPass}>
+              <label>Password</label>
+              <input type="password" placeholder="*****"></input>
+            </div>
+          </form>
+          <div className={classes.btnDiv}>
+            <button type="submit" className={classes.btn}>
+              Login
+            </button>
+          </div>
+          <div className={classes.signupDiv}>
+            <p className="new-text">OR</p>
+            <button type="submit" className={classes.btn}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Login;
