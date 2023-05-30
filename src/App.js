506a4144f353711175776df44d914ea8f4ddcb5c
Basit-Bashir <wbhat56@gmail.com>
2023-05-30 11:22:16 +0530

    Product details

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Login from "./Pages/authentication/Login/Login";
 import Signup from "./Pages/authentication/SignUp/Signup";
 import "./App.css";
 import Navbar from "./components/Header/Navbar";
-
+import Product from "./components/Products/Product";
 function App() {
   return (
     <div className="App">
       <Login />
       <Signup />
       <Navbar />
+      <Product />
     </div>
   );
 }
